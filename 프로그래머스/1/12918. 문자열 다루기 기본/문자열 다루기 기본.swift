55f537ab914995147b03e0dffa7fa342595fb77f
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-03-09 21:21:17 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.swift
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.swift
@@ -1,7 +1,7 @@
 func solution(_ s:String) -> Bool {
     if s.count == 4 || s.count == 6 {
         if let sInt = Int(String(s)) {
-        return true
+            return true
         }
     }
     return false 
