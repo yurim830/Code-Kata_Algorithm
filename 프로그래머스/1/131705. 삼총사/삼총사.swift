2af26a1146441760ff49c10ce6c7a4376c0ffdc3
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-03-14 11:47:02 +0900

    [level 1] Title: 삼총사, Time: 0.01 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.swift
+++ 프로그래머스/1/131705. 삼총사/삼총사.swift
@@ -4,7 +4,7 @@ func solution(_ number:[Int]) -> Int {
     var count: Int = number.count
     var trio : Int = 0
     for i in 0 ..< (count - 2) {
-        for j in (i+1) ..< count {
+        for j in (i+1) ..< (count - 1) {
             for k in (j+1) ..< count {
                 if number[i] + number[j] + number[k] == 0 {
                     trio += 1
@@ -12,6 +12,5 @@ func solution(_ number:[Int]) -> Int {
             }
         }
     }
-    
     return trio
 }
