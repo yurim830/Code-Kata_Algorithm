c83877bd0eb0276d572fdb3a7b6642bb688059dd
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-03-15 12:09:12 +0900

    [level 1] Title: 최소직사각형, Time: 24.71 ms, Memory: 18.8 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.swift
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.swift
@@ -5,17 +5,17 @@ func solution(_ sizes:[[Int]]) -> Int {
     var orderedSizes: [[Int]] = sizes.map { $0.sorted(by: >)}
     
     // 최대 너비, 높이 구하기
-    var w: Int = 0
-    var h: Int = 0
+    var maxW: Int = 0
+    var maxH: Int = 0
     
     for i in orderedSizes{
-        if i[0] > w {
-            w = i[0]
+        if i[0] > maxW {
+            maxW = i[0]
         }
-        if i[1] > h {
-            h = i[1]
+        if i[1] > maxH {
+            maxH = i[1]
         }
     }
     
-    return w * h
+    return maxW * maxH
 }
