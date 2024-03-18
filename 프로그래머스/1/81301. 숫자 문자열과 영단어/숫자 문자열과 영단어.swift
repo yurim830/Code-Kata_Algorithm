80b813d1fe7ca63cc7bd48148f11c68171e60d56
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-03-18 20:27:24 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.16 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.swift
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.swift
@@ -1,27 +1,10 @@
+import Foundation
+
 func solution(_ s:String) -> Int {
-    let numDict: [String:Int] = ["zero": 0, "one":1, "two":2, "three":3, "four":4, "five":5, "six":6, "seven":7, "eight":8, "nine":9 ]
-    let sArr: [Character] = Array(s)
-    var sIndex: Int = 0
-    var iArr: [String] = []
-    
-    while sIndex <= s.count - 1 {
-        if let temp = Int(String(sArr[sIndex])) {
-            iArr.append(String(temp))
-            sIndex += 1
-            
-        } else if let temp = numDict[String(sArr[sIndex..<(sIndex + 3)])] {
-            iArr.append(String(temp))
-            sIndex += 3
-            
-        } else if let temp = numDict[String(sArr[sIndex..<(sIndex + 4)])] {
-            iArr.append(String(temp))
-            sIndex += 4
-            
-        } else if let temp = numDict[String(sArr[sIndex..<(sIndex + 5)])] {
-            iArr.append(String(temp))
-            sIndex += 5
-        }
+    let arr = ["zero","one","two","three","four","five","six","seven","eight","nine"]
+    var str = s
+    for i in 0..<arr.count {
+        str = str.replacingOccurrences(of: arr[i], with: String(i))
     }
-    
-    return Int(iArr.joined())!
+    return Int(str)!
 }
