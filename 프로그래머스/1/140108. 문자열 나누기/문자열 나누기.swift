b5c7de53a690665c72bf5018856aee38967a07f6
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-03-19 09:37:54 +0900

    [level 1] Title: 문자열 나누기, Time: 20.28 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+func solution(_ s:String) -> Int {
+    var leftString: [Character] = Array(s)
+    var numOfSlice: Int = 0
+    
+    while leftString != [] {
+        numOfSlice += 1
+        execute(&leftString)
+    }
+    return numOfSlice
+}
+
+func execute(_ leftString: inout [Character]) {
+    let startS = leftString.removeFirst()
+    var same: Int = 1
+    var diff: Int = 0
+    while leftString != [] {
+        let target = leftString.removeFirst()
+        if startS == target {
+            same += 1
+        } else {
+            diff += 1
+        }
+        if same == diff{
+            break
+        }
+    }
+}
