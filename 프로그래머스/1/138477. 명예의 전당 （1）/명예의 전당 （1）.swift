d053664256f0c02d7228c7e68ff4543552b1c244
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-03-31 17:49:09 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.07 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.swift
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.swift
@@ -5,7 +5,7 @@ func solution(_ k:Int, _ score:[Int]) -> [Int] {
     var lowestScore: [Int] = []
     for (index, score) in score.enumerated() {
         hallOfFame.append(score)
-        hallOfFame.sort(by: {$0 > $1})
+        hallOfFame.sort(by: >)
         if hallOfFame.count > k {
             hallOfFame.popLast()
         }
