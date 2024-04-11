bce1eb095bc5e80fa6534dbda074baf0dabaf6b6
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-04-11 23:11:00 +0900

    [level 1] Title: 옹알이 (2), Time: 1.72 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.swift
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.swift
@@ -1,5 +1,22 @@
 import Foundation
 
 func solution(_ babbling:[String]) -> Int {
-    return 0
+    let possible: [String] = ["aya", "ye", "woo", "ma"]
+    let impossible: [String] = ["ayaaya", "yeye", "woowoo", "mama"]
+    var count: Int = 0
+    for word in babbling {
+        var word = word
+        for i in impossible {
+            word = word.replacingOccurrences(of: i, with: "0")
+        }
+        for i in possible {
+            word = word.replacingOccurrences(of: i, with: "1")
+        }
+        word = word.replacingOccurrences(of: "1", with: "")
+        if word.count == 0 {
+            count += 1
+            print(count)
+        }
+    }
+    return count
 }
