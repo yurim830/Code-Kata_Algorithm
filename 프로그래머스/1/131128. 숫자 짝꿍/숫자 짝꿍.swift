084378176f2547ca1251fe9c95f8bf5273898356
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-04-12 15:46:04 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.10 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+func solution(_ X:String, _ Y:String) -> String {
+    let xDict = makeNumDict(X)
+    let yDict = makeNumDict(Y)
+    
+    // mate 구하기
+    var mate: String = ""
+    for i in stride(from: 9, to: -1, by: -1) { // 9부터 0까지
+        mate += String(repeating: Character(String(i)), count: min(xDict[i]!, yDict[i]!))
+    }
+    
+    // mate 판별하기
+    if mate.isEmpty {
+        return "-1"
+    }
+    if mate.range(of: #"^0+$"#, options: .regularExpression) != nil {
+        return "0"
+    }
+    return mate
+}
+
+
+func makeNumDict(_ num: String) -> [Int:Int] {
+    var numDict: [Int:Int] = [:]
+    // 빈 사전 만들기
+    for i in 0...9 {
+        numDict[i] = 0
+    }
+    // 사전 값 넣기
+    for i in num {
+        if let count = numDict[Int(String(i))!] {
+            numDict[Int(String(i))!] = count + 1
+        }
+    }
+    return numDict
+}
