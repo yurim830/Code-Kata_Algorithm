0875e822903b92465fc59f876a75898bde4c8f1a
Yurim <157277372+yurim830@users.noreply.github.com>
2025-03-18 00:47:38 +0900

    [Silver IV] Title: 스택 2, Time: 948 ms, Memory: 94076 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.swift
+++ 백준/Silver/28278. 스택 2/스택 2.swift
@@ -1,27 +1,28 @@
 func doStack() {
     let numOfOrders = Int(readLine()!)!
     var stack: [Int] = []
-    var result: [Int] = []
-    
+    var results: [String] = []
+
     for _ in 0..<numOfOrders {
         let order = readLine()!
-        let splitedOrder = order.split(separator: " ")
-        
-        if order.count != 1 {
-            stack.append(Int(String(splitedOrder[1]))!)
+        let parts = order.split(separator: " ")
+
+        if parts[0] == "1", 
+           let value = Int(parts[1]) {
+            stack.append(value)
         } else if order == "2" {
             let item = stack.popLast() ?? -1
-            result.append(item)
+            results.append(String(item))
         } else if order == "3" {
-            result.append(stack.count)
+            results.append(String(stack.count))
         } else if order == "4" {
-            result.append(stack.isEmpty ? 1 : 0)
+            results.append(stack.isEmpty ? "1" : "0")
         } else if order == "5" {
-            result.append(stack.last ?? -1)
+            results.append(String(stack.last ?? -1))
         }
     }
-    
-    result.forEach { print($0) }
+
+    results.forEach { print($0) }
 }
 
 doStack()
